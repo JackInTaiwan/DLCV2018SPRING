dcd8399a7a1b2c7c9a64a2fa8c528ec1673d78f9
jackintaiwan <b04901081@ntu.edu.tw>
2018-04-28 13:21:28 +0800

    [M] data_preprocess.py mask_pic_to_npy()

--- hw3/utils/data_preprocess.py
+++ hw3/utils/data_preprocess.py
@@ -50,11 +50,12 @@ def sat_pic_to_npy(pic_dir_fp, output_fp, limit) :
 def mask_pic_to_npy(pic_dir_fp, output_fp, limit) :
     output = np.array([])
     total = len(os.listdir(pic_dir_fp)) / 2
-    for i in range(int(total)) :
-        if i >= limit :
+
+    for index in range(int(total)) :
+        if index >= limit :
             break
         else :
-            file = "{:0>4}_mask.png".format(i)
+            file = "{:0>4}_mask.png".format(index)
             img = plt.imread(os.path.join(pic_dir_fp, file))
             trans_v = np.array([1, 2, 4])
             # Here use transform vector
@@ -64,16 +65,18 @@ def mask_pic_to_npy(pic_dir_fp, output_fp, limit) :
             img_encoded = img.dot(trans_v)
             # we need to modify due to the TA's error.
             # there is no 1 (red) for our encoding, {0, 2, 3, 4, 5, 6, 7} only, so we do -1
-            img_encoded = img_encoded - 1 if img_encoded != 0 else 0
+            for i in range(img_encoded.shape[0]) :
+                for j in range(img_encoded.shape[1]) :
+                    img_encoded[i][j] = img_encoded[i][j] - 1 if img_encoded[i][j] != 0 else 0
 
-            print ("Num: {}/{} | File: {} | Size: {}".format(i, total, file, img.shape))
+            print ("Num: {}/{} | File: {} | Size: {}".format(index, total, file, img.shape))
 
             if len(output) == 0 :
                 output = np.array([img_encoded])
             else :
                 output = np.vstack((output, np.array([img_encoded])))
 
-
+    output = output.astype(np.int8)
     np.save(output_fp, output)
 
     print (
