0237aa3007f1589d7df1215f66d717678cf84e26
jackintaiwan <b04901081@ntu.edu.tw>
2018-04-29 12:01:19 +0000

    init

--- hw3/utils/train_tools.py
+++ hw3/utils/train_tools.py
@@ -20,15 +20,8 @@ def evaluate(model, x_eval, y_eval) :
     y_eval_var = Variable(y_eval).cuda()
     pred = model(x_eval_var)
     pred = tor.max(pred, 1)[1].cuda()
-<<<<<<< HEAD
-    correct = int((pred == y_var).data.sum())
-    total = int(y_var.size(0) * y_var.size(1) * y_var.size(2))
-    acc = correct / total
-=======
-
     correct = int((pred == y_eval_var).data.sum())
     total = int(y_eval_var.size(0) * y_eval_var.size(1) * y_eval_var.size(2))
     acc = round(correct / total, 5)
->>>>>>> e43997f82ccaa34b5769c98e09d2b6aa111fd79a
 
     return acc
