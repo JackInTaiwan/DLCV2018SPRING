e2b800ffbe6b9739b97d18feb7ab96cd54ddec5c
jackintaiwan <b04901081@ntu.edu.tw>
2018-05-05 08:33:56 +0000

    init

--- hw4/utils/train_tools.py
+++ hw4/utils/train_tools.py
@@ -19,19 +19,19 @@ def save_pic(save_fp, model, pic_n) :
     from torch.autograd import Variable
 
     for i in range(pic_n) :
-        file = "{0:>5}.png".format(i)
-        file_fp = os.path.join("./hw4_data", file)
-
+        f = "{:0>5}.png".format(i)
+        file_fp = os.path.join("./hw4_data/train", f)
+        print (file_fp)
         img = np.array([plt.imread(file_fp)]) / 255.
-        img_var = Variable(tor.FloatTensor(img))
+        img_var = Variable(tor.FloatTensor(img)).cuda()
         img_var = img_var.permute(0, 3, 1, 2)
-        out = model(img_var)
-        out = out.permute(0, 2, 3, 1)
-        out_img = out.data.numpy()
+        out, KLD = model(img_var)
+        out = out.permute(0, 2, 3, 1).cpu()
+        out_img = out.data.numpy()[0]
 
-        plt.imsave(os.path.join(save_fp, file), out_img)
+        plt.imsave(os.path.join(save_fp, f), out_img)
 
-        print ("|Output {} is saved.".format(file))
+        print ("|Output {} is saved.".format(f))
 
 
 
