4325acaec5b503a4acdf2ed86240d25fe137fdeb
jackintaiwan <b04901081@ntu.edu.tw>
2018-05-06 22:10:29 +0800

    [A] read_record.py

---
+++ hw4/read_record.py
@@ -0,0 +1,17 @@
+import json
+from argparse import ArgumentParser
+
+
+
+if __name__ == "__main__" :
+    parser = ArgumentParser()
+    parser.add_argument("-j", "--json", action="store", type=str, required=True, help="json file path")
+    parser.add_argument("-m", "--model", action="store", type=str, required=True, help="model name")
+
+    with open(parser.parse_args().json, "r") as f :
+        data = json.load(f)
+
+    data = json.loads(data)
+    for item in data :
+        if item["model_name"] == parser.parse_args().model :
+            print (item)
