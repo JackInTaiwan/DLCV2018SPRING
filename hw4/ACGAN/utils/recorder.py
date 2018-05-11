3137bab34d8d81d09233de9743a567783284988c
jackintaiwan <b04901081@ntu.edu.tw>
2018-05-11 22:23:58 +0800

    [A] ACGAN model is finished.

---
+++ hw4/ACGAN/utils/recorder.py
@@ -0,0 +1,40 @@
+import json
+
+
+def console(string) :
+    print("======== {} ========".format(string))
+
+
+
+def record(record_fp, datum) :
+    model_name = datum["model_name"]
+
+    with open(record_fp, "r") as f:
+        data = json.load(f)
+    data = json.loads(data)
+
+    for item in data :
+        if item["model_name"] == model_name :
+            item["lr"].append(datum["lr"])
+            item["loss"].append(datum["loss"])
+            item["acc_true"].append(datum["acc_true"])
+            item["acc_false"].append(datum["acc_false"])
+            break
+
+    else :
+        new_datum = dict()
+        new_datum["model_name"] = model_name
+        new_datum["data_size"] = datum["data_size"]
+        new_datum["batch_size"] = datum["batch_size"]
+        new_datum["decay"] = datum["decay"]
+        new_datum["lr_init"] = datum["lr_init"]
+        new_datum["lr"] = [datum["lr"]]
+        new_datum["record_epoch"] = datum["record_epoch"]
+        new_datum["loss"] = [datum["loss"]]
+        new_datum["acc_true"] = [datum["acc_true"]]
+        new_datum["acc_false"] = [datum["acc_false"]]
+        data.append(new_datum)
+
+    with open(record_fp, "w") as f :
+        data_json = json.dumps(data)
+        json.dump(data_json, f)
---
+++ hw4/ACGAN/utils/train_tools.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+
+
+def load_data(x_fp) :
+    x = np.load(x_fp)
+    x_size = x.shape
+
+    return x, x_size
+
+
+
+
+def save_pic(save_fp, model, pic_n) :
+    import cv2
+    import os
+    import time
+    import matplotlib.pyplot as plt
+    import torch as tor
+    from torch.autograd import Variable
+
+    for i in range(pic_n) :
+        #img = tor.randn((1, 512))
+        img = tor.randn(1, 512)
+        img_var = Variable(img).cuda()
+
+        out = model(img_var)
+
+        out = out.permute(0, 2, 3, 1).cpu()
+        out_img = out.data.numpy()[0] * 255
+        #print (out_img)
+        f = "{}_{:0>5}.png".format(int(time.time()), i)
+        plt.imsave(os.path.join(save_fp, f), out_img.astype(np.int16))
+
+        print ("|Output {} is saved.".format(f))
+
+
+
+
+def evaluate(model, x_eval, y_eval) :
+    import cv2
+    import torch as tor
+    from torch.autograd import Variable
+
+    correct = 0
+    x_eval_var = Variable(x_eval).type(tor.FloatTensor).cuda()
+    y_eval_var = Variable(y_eval).cuda()
+    for i in range(int(x_eval_var.size(0))) :
+        pred = model(x_eval_var[i: i+1])
+        pred = tor.max(pred, 1)[1].cuda()
+        correct += int((pred == tor.max(y_eval_var[i:i+1],1)[1]).data.sum())
+
+    total = int(y_eval_var.size(0) * y_eval_var.size(1) * y_eval_var.size(2))
+    acc = round(correct / total, 5)
+
+    return acc
+
+
+if __name__ == "__main__" :
+    import torch as tor
+    from model_GAN import GN
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument("--output", type=str, required=True)
+    parser.add_argument("--model", type=str, required=True)
+    parser.add_argument("-n", type=int, default=1)
+
+    model_fp = parser.parse_args().model
+    save_fp = parser.parse_args().output
+    n = parser.parse_args().n
+
+    model = GN()
+    model.cuda()
+    model.load_state_dict(tor.load(model_fp))
+    save_pic(save_fp, model, n)
