7e96ba1c98a9b806e880a3df6fdd336ccac3c616
jackintaiwan <b04901081@ntu.edu.tw>
2018-05-17 20:49:36 +0800

    bug

--- hw4/ACGAN/problems.py
+++ hw4/ACGAN/problems.py
@@ -103,7 +103,8 @@ def rand_generator(output_fp, model_fp) :
     xs[:, 0] = 0
 
     imgs = model(xs)
-    imgs = (imgs.permute(0, 2, 3, 1).cpu().data.numpy() / 2.0) + 0.5
+
+    imgs = ((imgs.permute(0, 2, 3, 1).cpu().data.numpy() / 2.0) + 0.5) * 255
     imgs = imgs.astype(np.int16)
 
     for i, img in enumerate(imgs, 1) :
