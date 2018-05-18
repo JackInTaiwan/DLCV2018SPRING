9cb798a32829f03d452cb1371012210b60d4000a
jackintaiwan <b04901081@ntu.edu.tw>
2018-05-18 16:47:11 +0800

    init

--- hw4/hw4.sh
+++ hw4/hw4.sh
@@ -8,14 +8,17 @@ mv ave_model_14.pkl ./VAE/models/
 mv gan_gn_fin.pkl ./GAN/models/
 mv acgan_gn_fin.pkl ./ACGAN/models/
 
-python3 ./VAE/problems.py -q tsne --output $2 --model ./VAE/models/ave_model_14.pkl --dataset $1
+echo 'Problem1...'
 python3 ./VAE/problems.py -q lcurve --output $2 --record ./VAE/record/vae_fin.json
-python3 ./VAE/problems.py -q rg --output $2 --model ./VAE/models/ave_model_14.pkl
 python3 ./VAE/problems.py -q test --output $2 --model ./VAE/models/ave_model_14.pkl --dataset $1
+python3 ./VAE/problems.py -q rg --output $2 --model ./VAE/models/ave_model_14.pkl
+python3 ./VAE/problems.py -q tsne --output $2 --model ./VAE/models/ave_model_14.pkl --dataset $1
 
+echo 'Problem1...'
 python3 ./GAN/problems.py -q lcurve --output $2 --record ./GAN/record/gan_fin.json
 python3 ./GAN/problems.py -q rg --output $2 --model ./GAN/models/gan_gn_fin.pkl
 
+echo 'Problem1...'
 python3 ./ACGAN/problems.py -q lcurve --output $2 --record ./ACGAN/record/acgan_fin.json
 python3 ./ACGAN/problems.py -q rg --output $2 --model ./ACGAN/models/acgan_gn_fin.pkl
 
