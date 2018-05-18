35fb1491fc2a08febb26f9bf8b8aef27a6fc5d4c
jackintaiwan <b04901081@ntu.edu.tw>
2018-05-18 17:03:48 +0800

    init

--- hw4/hw4.sh
+++ hw4/hw4.sh
@@ -14,11 +14,11 @@ python3 ./VAE/problems.py -q test --output $2 --model ./VAE/models/ave_model_14.
 python3 ./VAE/problems.py -q rg --output $2 --model ./VAE/models/ave_model_14.pkl
 python3 ./VAE/problems.py -q tsne --output $2 --model ./VAE/models/ave_model_14.pkl --dataset $1
 
-echo 'Problem1...'
+echo 'Problem2...'
 python3 ./GAN/problems.py -q lcurve --output $2 --record ./GAN/record/gan_fin.json
 python3 ./GAN/problems.py -q rg --output $2 --model ./GAN/models/gan_gn_fin.pkl
 
-echo 'Problem1...'
+echo 'Problem3...'
 python3 ./ACGAN/problems.py -q lcurve --output $2 --record ./ACGAN/record/acgan_fin.json
 python3 ./ACGAN/problems.py -q rg --output $2 --model ./ACGAN/models/acgan_gn_fin.pkl
 
