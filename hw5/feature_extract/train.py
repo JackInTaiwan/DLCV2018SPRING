b985effa644cf4b2ba0213ab15d407cea155b0b8
jackintaiwan <b04901081@ntu.edu.tw>
2018-05-28 20:19:45 +0800

    init

--- hw5/feature_extract/train.py
+++ hw5/feature_extract/train.py
@@ -177,12 +177,13 @@ def train(model, model_index, limit, valid_limit) :
                     print ("|Acc on train data: {}".format(round(acc_train, 5)))
                     print ("|Acc on test data: {}".format(round(acc_test, 5)))
 
+            if epoch % SAVE_MODEL_PERIOD == 0:
+                save_model_fp = os.path.join(MODEL_FP, "model_{}".format(model_index))
+                model.save(save_model_fp)
 
         model.run_epoch()
 
-        if epoch % SAVE_MODEL_PERIOD == 0 :
-            save_model_fp = os.path.join(MODEL_FP, "model_{}".format(model_index))
-            model.save(save_model_fp)
+
 
 
 
