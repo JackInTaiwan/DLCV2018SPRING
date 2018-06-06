a7f1b4e5184927a1666db9b26f1e155d5255f635
jackintaiwan <b04901081@ntu.edu.tw>
2018-06-06 23:57:01 +0800

    init

--- hw5/Seq2seq/train.py
+++ hw5/Seq2seq/train.py
@@ -161,7 +161,8 @@ def train(model, model_index, limit, valid_limit) :
                 """
                 loss = 0
                 for _i, o in enumerate(output) :
-                    print ("!!")
+                    print (o.unsqueeze(0))
+                    print (y[0][_i].unsqueeze(0))
                     loss = loss + loss_func(o.unsqueeze(0), y[0][_i].unsqueeze(0))
 
                 loss = loss / (_i + 1)
