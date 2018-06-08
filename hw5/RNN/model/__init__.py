717cc4289478446ca95cd9471889394ee707fd01
jackintaiwan <b04901081@ntu.edu.tw>
2018-06-08 22:01:03 +0800

    init

--- hw5/RNN/model/__init__.py
+++ hw5/RNN/model/__init__.py
@@ -1,5 +1,6 @@
 from .model import Classifier
 from .model import RNN as RNN
+from .model import RNN as RNN_1
 from .model_2 import RNN as RNN_2
 from .model_3 import RNN as RNN_3
 from .model_4 import RNN as RNN_4
