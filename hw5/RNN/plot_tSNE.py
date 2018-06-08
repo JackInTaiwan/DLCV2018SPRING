1be64692811638ed164f7da772eb37d006ed75dc
jackintaiwan <b04901081@ntu.edu.tw>
2018-06-08 22:33:11 +0800

    init

--- hw5/RNN/plot_tSNE.py
+++ hw5/RNN/plot_tSNE.py
@@ -1,8 +1,15 @@
 import cv2
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 import torch as tor
+
 from argparse import ArgumentParser
+from sklearn.manifold.t_sne import TSNE
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 
 
@@ -14,17 +21,18 @@ LABELS_TEST_FP = "./labels_valid_0.npy"
 
 
 
-def plot_tsne(mode, model_fp, limit) :
+def plot_tsne(model_fp, output_fp, limit) :
 
     videos = np.load(VIDEOS_TEST_FP)
     labels = np.load(LABELS_TEST_FP)
 
-
-    model = tor.load(model_fp).cuda()
+    model = tor.load(model_fp)
+    model.cuda()
 
     correct, total = 0, len(labels)
 
     features_rnn = []
+    labels = []
 
     for i, (x, label) in enumerate(zip(videos, labels), 1) :
         print ("Process: {}/{}".format(i, total))
@@ -32,19 +40,34 @@ def plot_tsne(mode, model_fp, limit) :
         f = model.get_feature(x)
         features_rnn.append(f)
 
-    print (features_rnn)
+        labels.append(int(label))
+
+
+    ### tSNE
+    tsne = TSNE(
+        n_components=2,
+        random_state=0,
+    )
+    f_tsne = tsne.fit_transform(features_rnn)
+
+    plt.scatter(f_tsne[labels == 0, 0], f_tsne[labels == 0, 1], c="r")
+    plt.scatter(f_tsne[labels == 1, 0], f_tsne[labels == 1, 1], c="b")
+    #plt.legend(["Not {}".format(attr_selected), attr_selected])
+
+    plt.savefig(os.path.join(output_fp, "tSNE_RNN.jpg"))
 
 
 
 
 if __name__ == "__main__" :
     parser = ArgumentParser()
-    parser.add_argument("-m", type=str, required=True, choices=["valid", "train"])
     parser.add_argument("-l", type=int, default=None, help="limitation of amount of data")
     parser.add_argument("--load", type=str, required=True, help="loaded model file path")
+    parser.add_argument("--output", type=str, default=None)
 
     mode = parser.parse_args().m
     model_fp = parser.parse_args().load
+    output_fp = parser.parse_args().output
     limit = parser.parse_args().l
 
-    plot_tsne(mode, model_fp, limit)
+    plot_tsne(model_fp, output_fp, limit)
