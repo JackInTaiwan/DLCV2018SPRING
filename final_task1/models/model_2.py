73fd03ff18d57270e0fcf324ffb2236f27133ab0
jackintaiwan <b04901081@ntu.edu.tw>
2018-06-21 01:09:32 +0800

    init

--- final_task1/models/model_2.py
+++ final_task1/models/model_2.py
@@ -36,9 +36,9 @@ class RelationNet(nn.Module) :
             #nn.MaxPool2d(kernel_size=2),
         )
 
-        self.vgg16_dense = self.fc(vgg16_dense_chls[0], vgg16_dense_chls[1], sig=True, relu=False)
+        self.vgg16_dense = self.fc(vgg16_dense_chls[0], vgg16_dense_chls[1], relu=False)
 
-        score_dense_chls = [vgg16_dense_chls[-1] * 2, 2 ** 9, 2 ** 9, 1]
+        score_dense_chls = [vgg16_dense_chls[-1] * 2, 2 ** 10, 2 ** 10, 1]
 
         self.score_dense = nn.Sequential(
             self.fc(score_dense_chls[0], score_dense_chls[1]),
