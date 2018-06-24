6803a42426c06258205cc31bdf393eeb72c69479
jackintaiwan <b04901081@ntu.edu.tw>
2018-06-25 01:00:50 +0800

    init

--- final_task1/train_4.sh
+++ final_task1/train_4.sh
@@ -7,3 +7,12 @@ python3 main.py -i 71 --version 13 --way 5 --shot 5 --lr 0.00001 --record ./reco
 echo "========== 72 =========="
 python3 main.py -i 72 --version 14 --way 5 --shot 5 --lr 0.00001 --record ./records/relationnet --step 50000
 
+echo "========== 73 =========="
+python3 main.py -i 70 --version 12 --way 5 --shot 5 --lr 0.000005 --record ./records/relationnet --step 50000
+
+echo "========== 74 =========="
+python3 main.py -i 71 --version 13 --way 5 --shot 5 --lr 0.000005 --record ./records/relationnet --step 50000
+
+echo "========== 75 =========="
+python3 main.py -i 72 --version 14 --way 5 --shot 5 --lr 0.000005 --record ./records/relationnet --step 50000
+
