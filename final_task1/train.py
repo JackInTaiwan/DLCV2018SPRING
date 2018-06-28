098604a2783811cc7271d0dea38c7158b97657e5
jackintaiwan <b04901081@ntu.edu.tw>
2018-06-28 10:26:47 +0800

    init

--- final_task1/train.py
+++ final_task1/train.py
@@ -56,7 +56,7 @@ class Trainer :
 
         x = self.base_train[way_pick][:, shot_pick[:-5]]
 
-        x_query = self.base_train[way_pick][:, shot_pick[-5:]]
+        x_query = self.base_train[way_pick][:, shot_pick[-5:]].reshape(self.way * 5, 32, 32, 3)
         y_query = np.array([i // 5 for i in range(self.way * 5)])
 
         return  x, x_query, y_query
@@ -96,12 +96,9 @@ class Trainer :
 
             x, x_query, y_query_idx = self.dump_novel_train()
             x = tor.Tensor(x).permute(0, 1, 4, 2, 3)
-            print (x_query.shape)
-            print (x_query.ndim)
             x_query = tor.Tensor(x_query).unsqueeze(0).permute(0, 3, 1, 2) if x_query.ndim == 3 else tor.Tensor(x_query).permute(0, 3, 1, 2)
             y_query = tor.zeros(self.way * 5 * 5, 1)
             y_query[y_query_idx] = 1
-            print (y_query)
 
             if not self.cpu :
                 x, x_query, y_query = x.cuda(), x_query.cuda(), y_query.cuda()
