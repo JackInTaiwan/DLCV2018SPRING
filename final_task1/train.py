1b22b7f5ac07f68a0b75560073cbd3b0fc5bb6b0
jackintaiwan <b04901081@ntu.edu.tw>
2018-06-28 07:05:18 +0000

    init

--- final_task1/train.py
+++ final_task1/train.py
@@ -107,7 +107,7 @@ class Trainer :
             scores = self.model(x, x_query)
             # calculate training accuracy
             acc = tor.argmax(scores.view(25, 5), dim=1) == tor.LongTensor(np.array([i // 5 for i in range(25)])).cuda()
-            acc = tor.mean(acc)
+            acc = acc.cpu().data.numpy()
             print (acc)
             train_acc_list.append(float(acc.cpu()))
 
