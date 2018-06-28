c3e621a4d37db9bd7a2ca2c227adcfb6397b8c2f
jackintaiwan <b04901081@ntu.edu.tw>
2018-06-28 18:54:51 +0800

    init

--- final_task1/models/model_23.py
+++ final_task1/models/model_23.py
@@ -107,7 +107,7 @@ class RelationNet(nn.Module) :
 
             cat = tor.cat([x, x_query], 1)
             score = self.score_dense(cat)
-            score = tor.mean(score.view(5, 5, 5), dim=2)
+            score = tor.mean(score.view(25, 5, 5), dim=2)
 
             return score
 
