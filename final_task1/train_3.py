c9ba9c01e3f49c8d76bb04bdc0a748d800dc40e0
jackintaiwan <b04901081@ntu.edu.tw>
2018-06-30 01:03:10 +0800

    init

--- final_task1/train_3.py
+++ final_task1/train_3.py
@@ -77,7 +77,7 @@ class Trainer:
 
             x = self.novel_support
             x = tor.Tensor(x).permute(0, 1, 4, 2, 3).view(-1, 3, 32, 32)
-            y = tor.Tensor(np.array([i // 20 for i in range(20 * 5)])).view(-1, 1)
+            y = tor.LongTensor(np.array([i // 20 for i in range(20 * 5)])).view(-1, 1)
 
             if not self.cpu:
                 x, y = x.cuda(), y.cuda()
