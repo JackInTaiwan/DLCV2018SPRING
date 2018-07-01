8d0e0bbd370e1abadb86072c90c14f8c99ddf555
jackintaiwan <b04901081@ntu.edu.tw>
2018-07-01 11:55:15 +0800

    init

---
+++ final_task1/seed.sh
@@ -0,0 +1,5 @@
+for i in {20..40}
+do
+    echo "========== $i =========="
+    python3 main.py -i 140 --net classifier --version 31 --trainer 3 --record records/classifier/ --load records/classifier/classifier_140.json --step 100000 --lr 0.0 --seed $i --step 1
+done
