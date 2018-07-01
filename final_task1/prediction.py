fc0c33d823a5085f44470d7ef2135efa28d59d3c
jackintaiwan <b04901081@ntu.edu.tw>
2018-07-01 19:57:56 +0800

    init

--- final_task1/prediction.py
+++ final_task1/prediction.py
@@ -114,7 +114,7 @@ def evaluation(model, support_data, data_fp, output_fp) :
         #img = img * 225.
         img = tor.Tensor(img).view(1, 32, 32, 3).permute(0, 3, 1, 2).cuda()
         pred = model.pred(support_data, img)
-        pred_list.append([i, table[int(pred[0])]])
+        pred_list.append([i, "{:0>2}".format(table[int(pred[0])])])
 
     pred_df = pd.DataFrame(pred_list)
     pred_df.to_csv(output_fp, header=["image_id", "predicted_label"], index=None)
