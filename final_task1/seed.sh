d68776e223989e6e15f63e74f77095790be0201a
jackintaiwan <b04901081@ntu.edu.tw>
2018-07-01 17:03:40 +0800

    init

--- final_task1/seed.sh
+++ final_task1/seed.sh
@@ -1,5 +1,5 @@
-for i in {41..100}
+for i in {1..100}
 do
     echo "========== $i =========="
-    python3 main.py -i 140 --net classifier --version 31 --trainer 3 --record records/classifier/ --load records/classifier/classifier_140.json --step 100000 --lr 0.0 --seed $i --step 1
+    python3 main.py -i 153 --net classifier --version 31 --trainer 4 --record records/classifier/ --load records/classifier/classifier_153.json --step 100000 --lr 0.00000001 --seed $i
 done
