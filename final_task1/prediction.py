0cb7f9dfc087ae4bd0124e93a78f664520d5a0af
jackintaiwan <b04901081@ntu.edu.tw>
2018-07-01 17:55:29 +0800

    init

--- final_task1/prediction.py
+++ final_task1/prediction.py
@@ -113,7 +113,7 @@ def evaluation(model, support_data, data_fp, output_fp) :
         img = (img - 0.5) * 2
         #img = img * 225.
         img = tor.Tensor(img).view(1, 32, 32, 3).permute(0, 3, 1, 2).cuda()
-        pred = model(support_data, img)
+        pred = model.pred(support_data, img)
         pred = tor.argmax(pred, dim=0).cpu()
         pred_list.append([i, table[int(pred)]])
 
