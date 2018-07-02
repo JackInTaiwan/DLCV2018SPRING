f79ad086b2e52a1f9cc6b7c3648e146cd9ec6396
jackintaiwan <b04901081@ntu.edu.tw>
2018-07-02 21:29:32 +0800

    init

--- final_task1/models/__init__.py
+++ final_task1/models/__init__.py
@@ -35,7 +35,7 @@ from .model_34 import Classifier as model_34
 from .model_35 import Classifier as model_35
 from .model_36 import Classifier as model_36
 from .model_37 import Classifier as model_37
-
+from .model_38 import Classifier as model_38
 
 
 MODELS = [
@@ -76,4 +76,5 @@ MODELS = [
     model_35,
     model_36,
     model_37,
+    model_38,
 ]
